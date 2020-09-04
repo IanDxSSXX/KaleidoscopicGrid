8756ca12040d4f1731a6df1451d8b6b045b7faa7
Ian Dx <IanDxSSXX@gmail.com>
2020-09-04 16:12:06 +0800

    Update

--- KaleidoscopicGrid/KaleidoscopicGrid/GridView.swift
+++ KaleidoscopicGrid/KaleidoscopicGrid/GridView.swift
@@ -27,9 +27,9 @@ struct GridView: View {
     var onTap: () -> Void = { return }
     
     /// 根据列数获得宽，并监听设备旋转
-    @State private var width: CGFloat = 0
+    @State var width: CGFloat = 0
     /// 因为有奇奇怪怪的问题，所以经过测试要一个first，问题在于第一次进入通知中心就会发通知
-    @State private var first: Bool = true
+    @State var first: Bool = true
     
     /// 初始化
     init(gridType: GridType = .Default, custmizedViews: [AnyView] = [], images: [Image] = [], titles: [String] = [], discriptions: [String] = [], displayType: DisplayType = .Bigger, columns: Int = 0, onTap: @escaping () -> Void = { return }) {
@@ -75,11 +75,10 @@ struct GridView: View {
             if gridType == .Default {
                 /// 默认的颜色块
                 SubView(
-                    views: getColorBlock(
-                        width: width,
-                        padding: padding
-                    ),
+                    views: getColorBlock(),
                     columns: columns,
+                    width: width,
+                    pad: padding,
                     onTap: onTap
                 )
             }else if gridType == .Customized {
@@ -87,17 +86,19 @@ struct GridView: View {
                 SubView(
                     views: custmizedViews,
                     columns: columns,
+                    width: width,
+                    pad: padding,
                     onTap: onTap
                 )
             }else if gridType == .Image {
                 /// 只显示图片的模版
                 SubView(
                     views: getImageView(
-                        images: images,
-                        width: width,
-                        padding: padding
+                        images: images
                     ),
                     columns: columns,
+                    width: width,
+                    pad: padding,
                     onTap: onTap
                 )
             }else if gridType == .ImageTitle {
@@ -105,11 +106,11 @@ struct GridView: View {
                 SubView(
                     views: getImageTitleView(
                         images: images,
-                        titles: titles,
-                        width: width,
-                        padding: padding
+                        titles: titles
                     ),
                     columns: columns,
+                    width: width,
+                    pad: padding,
                     onTap: onTap
                 )
             }else if gridType == .ImageTitleDiscription {
@@ -118,11 +119,11 @@ struct GridView: View {
                     views: getImageTitleDiscriptionView(
                         images: images,
                         titles: titles,
-                        discriptions: discriptions,
-                        width: width,
-                        padding: padding
+                        discriptions: discriptions
                     ),
                     columns: columns,
+                    width: width,
+                    pad: padding,
                     onTap: onTap
                 )
             }
@@ -152,19 +153,27 @@ struct GridView: View {
 
 /// 含stack的包装view
 struct SubView: View {
-    var views: [AnyView]
+    var views: [AnyView] = []
     var columns: Int
+    var width: CGFloat
+    var pad: CGFloat
     var onTap: () -> Void
+    
     var body: some View {
         ScrollView(.vertical) {
             LazyHStack(alignment: .top, spacing: 0) {
                 ForEach(0..<columns) { column in
                     LazyVStack(spacing: 0) {
-                        ForEach(0..<Int(views.count / columns)) { item in
+                        ForEach(0..<Int(views.count / columns + 1), id: \.self) { item in
                             if Int(item * columns + column) < views.count {
                                 views[Int(item * columns + column)]
                                     .onTapGesture{
                                         onTap()
+                                    }
+                                    .frame(width: width)
+                                    .padding(.all, pad)
+                                    .onAppear {
+                                        print(Int(item * columns + column))
                                     }
                             }
                         }
@@ -176,20 +185,19 @@ struct SubView: View {
 }
 
 /// 得到不同颜色的块，默认展示用
-func getColorBlock(width: CGFloat, padding: CGFloat) -> [AnyView] {
+func getColorBlock() -> [AnyView] {
     let colors: [Color] = [.blue, .gray, .green, .orange, .pink, .purple, .red, .yellow]
     var views: [AnyView] = []
-    for i in 0..<1000 {
+    for i in 0..<10 {
         views.append(
             AnyView(ZStack {
                 RoundedRectangle(cornerRadius: 10)
-                    .frame(width: width, height: 100 + CGFloat(arc4random_uniform(20) * 10))
+                    .frame(height: 100 + CGFloat(arc4random_uniform(20) * 10))
                     .foregroundColor(colors[Int(arc4random_uniform(8))])
                 Text("\(i)")
                     .font(.title)
                     .foregroundColor(.white)
             }
-            .padding(.all, padding)
             )
         )
     }
@@ -197,11 +205,11 @@ func getColorBlock(width: CGFloat, padding: CGFloat) -> [AnyView] {
 }
 
 /// 得到不同的光图片的View
-func getImageView(images: [Image], width: CGFloat, padding: CGFloat) -> [AnyView] {
+func getImageView(images: [Image]) -> [AnyView] {
     var views: [AnyView] = []
     for image in images {
         views.append(
-            AnyView(ImageView(image: image, width: width, padding: padding))
+            AnyView(ImageView(image: image))
         )
     }
     
@@ -209,11 +217,11 @@ func getImageView(images: [Image], width: CGFloat, padding: CGFloat) -> [AnyView
 }
 
 /// 得到不同的图片和大标题的view
-func getImageTitleView(images: [Image], titles: [String], width: CGFloat, padding: CGFloat) -> [AnyView] {
+func getImageTitleView(images: [Image], titles: [String]) -> [AnyView] {
     var views: [AnyView] = []
     for (image, title) in zip(images, titles) {
         views.append(
-            AnyView(ImageTitleView(image: image, title: title, width: width, padding: padding))
+            AnyView(ImageTitleView(image: image, title: title))
         )
     }
     
@@ -221,11 +229,11 @@ func getImageTitleView(images: [Image], titles: [String], width: CGFloat, paddin
 }
 
 /// 得到不同的图片、标题和详情的view
-func getImageTitleDiscriptionView(images: [Image], titles: [String], discriptions: [String], width: CGFloat, padding: CGFloat) -> [AnyView] {
+func getImageTitleDiscriptionView(images: [Image], titles: [String], discriptions: [String]) -> [AnyView] {
     var views: [AnyView] = []
     for ((image, title), discription) in zip(zip(images, titles), discriptions) {
         views.append(
-            AnyView(ImageTitleDiscriptionView(image: image, title: title, discription: discription, width: width, padding: padding))
+            AnyView(ImageTitleDiscriptionView(image: image, title: title, discription: discription))
         )
     }
     return views
@@ -235,19 +243,15 @@ func getImageTitleDiscriptionView(images: [Image], titles: [String], discription
 /// 仅图片的view
 struct ImageView: View {
     var image: Image
-    var width: CGFloat
-    var padding: CGFloat
 
     var body: some View {
         VStack(spacing: 0) {
             image
                 .resizable()
                 .scaledToFit()
-                .frame(width: width)
         }
         .clipShape(RoundedRectangle(cornerRadius: 10))
         .overlay(RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 2).foregroundColor(.secondary))
-        .padding(.all, padding)
         .frame(maxHeight: .infinity)
     }
 }
@@ -256,27 +260,23 @@ struct ImageView: View {
 struct ImageTitleView: View {
     var image: Image
     var title: String
-    var width: CGFloat
-    var padding: CGFloat
 
     var body: some View {
         VStack(spacing: 0) {
             image
                 .resizable()
                 .scaledToFit()
-                .frame(width: width)
             Rectangle()
-                .frame(width: width, height: 2)
+                .frame(height: 2)
                 .foregroundColor(.secondary)
             Text(title)
                 .font(.title)
                 .background(Color.white)
                 .padding(.all, 6)
-                .frame(width: width, alignment: .leading)
+                .frame(alignment: .leading)
         }
         .clipShape(RoundedRectangle(cornerRadius: 10))
         .overlay(RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 2).foregroundColor(.secondary))
-        .padding(.all, padding)
         .frame(maxHeight: .infinity)
     }
 }
@@ -286,34 +286,30 @@ struct ImageTitleDiscriptionView: View {
     var image: Image
     var title: String
     var discription: String
-    var width: CGFloat
-    var padding: CGFloat
 
     var body: some View {
         VStack(spacing: 0) {
             image
                 .resizable()
                 .scaledToFit()
-                .frame(width: width)
             Rectangle()
-                .frame(width: width, height: 2)
+                .frame(height: 2)
                 .foregroundColor(.secondary)
             Text(title)
                 .font(.title)
                 .background(Color.white)
                 .padding(.all, 6)
-                .frame(width: width, alignment: .leading)
+                .frame(alignment: .leading)
                 .foregroundColor(.primary)
             Text(discription)
                 .font(.body)
                 .background(Color.white)
                 .padding(.all, 6)
-                .frame(width: width, alignment: .leading)
+                .frame(alignment: .leading)
                 .foregroundColor(.secondary)
         }
         .clipShape(RoundedRectangle(cornerRadius: 10))
         .overlay(RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 2).foregroundColor(.secondary))
-        .padding(.all, padding)
         .frame(maxHeight: .infinity)
     }
 }
@@ -322,6 +318,6 @@ struct ImageTitleDiscriptionView: View {
 
 struct Grid_Previews: PreviewProvider {
     static var previews: some View {
-        GridView(gridType: .Image, images: getImages())
+        GridView(displayType: .Bigger)
     }
 }
